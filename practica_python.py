816ca3202115c739f32fc105a876c04b0a77fe10
Jeniffer Collazos <jecollazosdev@gmail.com>
2026-06-11 19:46:30 -0500

    pandas basico y primer ETL

--- practica_python.py
+++ practica_python.py
@@ -1,17 +1,9 @@
-def filtrar_por_ciudad(clientes, ciudad):
-    resultado = []
-    for cliente in clientes:
-        if cliente["ciudad"] == ciudad:
-            resultado.append(cliente["nombre"])
-    return resultado
+import pandas as pd
+df = pd.read_csv('transacciones.csv')
 
-clientes = [
-    {"nombre": "Ana Torres", "ciudad": "Cali", "saldo": 1500000},
-    {"nombre": "Carlos Ruiz", "ciudad": "Bogotá", "saldo": 3200000},
-    {"nombre": "María López", "ciudad": "Medellín", "saldo": 850000},
-    {"nombre": "Pedro Gómez", "ciudad": "Cali", "saldo": 4100000},
-    {"nombre": "Lucía Herrera", "ciudad": "Bogotá", "saldo": 620000}
-]
+retiros = df[df['tipo'] == 'retiro']
 
-print(filtrar_por_ciudad(clientes, "Cali"))
-print(filtrar_por_ciudad(clientes, "Bogotá"))
+retiros.to_csv('retiros.csv, index=False')
+
+print('ETL completado')
+print(retiros)
