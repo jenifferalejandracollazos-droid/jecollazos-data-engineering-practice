fde6a99e3c497b674bc22c83c9e57b20ea0dba6f
Jeniffer Collazos <jecollazosdev@gmail.com>
2026-07-02 22:58:06 -0500

    repaso SQL: DISTINCT Y CTEs

--- practica_python.py
+++ practica_python.py
@@ -1,7 +1,31 @@
-import pandas as pd 
+1.
+SELECT nombre, saldo 
+FROM clientes
+WHERE saldo > (SELECT AVG(saldo) FROM clientes)
+ORDER BY saldo DESC;
 
-df = pd.read_csv('bank_transactions_data_2_augmented_clean_2.csv')
+2.
+SELECT cliente_id, COUNT(tipo)
+FROM transacciones
+GROUP BY cliente_id
+ORDER BY COUNT(tipo) DESC;
 
-filtro_saldo_promedio = df.groupby('CustomerOccupation')['AccountBalance'].mean()
+3. 
+SELECT clientes.nombre, SUM(transacciones.monto)
+FROM clientes 
+INNER JOIN transacciones ON clientes.id = transacciones.cliente_id
+GROUP BY clientes.nombre
+HAVING SUM(transacciones.monto) > 500000
 
-print(filtro_saldo_promedio)
+4. 
+SELECT clientes.id, transacciones.tipo
+FROM clientes
+INNER JOIN transacciones ON clientes.id = transacciones.cliente_id
+WHERE clientes.id NOT IN ('transacciones')
+
+5.
+SELECT clientes.nombre, clientes.ciudad, transacciones.monto
+FROM clientes
+INNER JOIN transacciones ON clientes.id = transacciones.cliente_id 
+WHERE clientes.ciudad = 'Bogotá'
+GROUP BY clientes.nombre
